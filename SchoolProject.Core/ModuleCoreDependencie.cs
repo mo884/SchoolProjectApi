4ffe085cda4054aeb6f34dcee75452e3c192f3f4
mohamedessam77740@gmail.com <mohamedessam77740@gmail.com>
2023-07-28 23:29:58 +0200

    add in module AddMediatR

--- SchoolProject.Core/ModuleCoreDependencie.cs
+++ SchoolProject.Core/ModuleCoreDependencie.cs
@@ -1,7 +1,16 @@
-﻿namespace SchoolProject.Core
-{
-	public class ModuleCoreDependencie
-	{
+﻿using Microsoft.Extensions.DependencyInjection;
+using SchoolProject.Infrustructure.Abstraction;
+using SchoolProject.Infrustructure.Repesiratories;
+using System.Reflection;
 
+namespace SchoolProject.Core
+{
+	public static class ModuleCoreDependencie
+	{
+		public static IServiceCollection AddCoreDependencie(this IServiceCollection services)
+		{
+			services.AddMediatR(a => a.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+			return services;
+		}
 	}
 }
