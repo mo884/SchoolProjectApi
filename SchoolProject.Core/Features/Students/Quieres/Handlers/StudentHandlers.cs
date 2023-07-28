771c24a06bebee07badd6f89299864491108d499
mohamedessam77740@gmail.com <mohamedessam77740@gmail.com>
2023-07-28 23:26:45 +0200

    add Handler request in Core Layer

---
+++ SchoolProject.Core/Features/Students/Quieres/Handlers/StudentHandlers.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SchoolProject.Core.Features.Students.Quieres.Models;
+using SchoolProject.Data.Entites;
+using SchoolProject.Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Core.Features.Students.Quieres.Handlers
+{
+	public class StudentHandlers : IRequestHandler<GetStudentListQuerey, List<Student>>
+	{
+		#region Fieldes
+		private readonly IStudentServies studentServies;
+
+		#endregion
+
+		#region Constructor
+		public StudentHandlers(IStudentServies studentServies)
+        {
+			this.studentServies = studentServies;
+		}
+        #endregion
+
+        #region Handles  Function
+        #endregion
+        public async Task<List<Student>> Handle(GetStudentListQuerey request, CancellationToken cancellationToken)
+		{
+			return await studentServies.GetStudentsListAsync();
+		}
+	}
+}
--- SchoolProject.Core/Features/Students/Quieres/Models/GetStudentListQuerey.cs
+++ SchoolProject.Core/Features/Students/Quieres/Models/GetStudentListQuerey.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SchoolProject.Data.Entites;
 
 namespace SchoolProject.Core.Features.Students.Quieres.Models
 {
-	internal class GetStudentListQuerey
+	public class GetStudentListQuerey:IRequest<List<Student>>
 	{
 	}
 }
